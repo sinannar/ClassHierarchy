29495b5ec1fad236d6323a134afe86cdaec143dc
Sinan NAR <sinan.nar@gmail.com>
2018-01-10 10:38:14 +1300

    more examples has been implemented

---
+++ ClassHierarchy/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassHierarchy
+{
+    class Rectangle
+    {
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public virtual int Height { get; set; }
+        public virtual int Width { get; set; }
+        public int Area
+        {
+            get { return Height * Width; }
+        }
+    }
+}
---
+++ ClassHierarchy/Square.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassHierarchy
+{
+    class Square : Rectangle
+    {
+        public Square(int size) : base(size, size)
+        {
+        }
+
+        public override int Width
+        {
+            get { return base.Width; }
+            set
+            {
+                base.Width = value;
+                base.Height = value;
+            }
+        }
+
+        public override int Height
+        {
+            get { return base.Height; }
+            set
+            {
+                base.Height = value;
+                base.Width = value;
+            }
+        }
+    }
+
+    public class Test
+    {
+        public void foo()
+        {
+            Rectangle rectangle = new Square(1);
+            rectangle.Width = 10;
+            rectangle.Height = 5;
+
+            Console.WriteLine(rectangle.Area);//This code will output 25.
+        }
+    }
+}
